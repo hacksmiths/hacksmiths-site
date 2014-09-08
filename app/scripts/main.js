6bcba59c012347b37298dacbca2263f4b492bf78
hughrawlinson <hugh@codeoclock.net>
2014-09-08 17:01:55 +0100

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,76 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
+        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
+        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
+        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
+        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
+        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
+        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
+        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
+        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
+        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition'
+    },
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery']
+        },
+        bootstrapPopover: {
+            deps: ['jquery']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        },
+        'jquery.stellar':{
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'jquery', 'prompt','jquery.stellar'], function (app, $, prompt) {
+    'use strict';
+    // use app here
+    prompt();
+    if(!Modernizr.touch){
+
+        $.stellar();
+
+        $(document).on('scroll ontouchmove', function(){
+            var colorOffset = (
+                $(document).scrollTop()/(
+                    $(document).height()-
+                    $('html').height()
+                )
+            );
+/*
+            $('html').css('background',
+                'hsl('+(110+colorOffset*120)+',100%,80%)'
+            );
+*/
+        });
+    }
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
---
+++ app/scripts/prompt.js
@@ -0,0 +1,20 @@
+/*global define */
+define(['jquery'], function ($) {
+	'use strict';
+
+	return function(){
+		var now = new Date();
+		var self = {
+			text:"Last login: "+now.toString()+" on ttys001<br>~ <span class=\"red\">❯</span><span class=\"green\">❯</span><span class=\"blue\">❯</span> ",
+			update:function(){
+				$('#prompt').html(self.text+(self.underscore?'_':''));
+				console.log(now.toString());
+				self.underscore=!self.underscore;
+			},
+			rate:1000,
+			underscore:true
+		}
+		self.update();
+		setInterval(self.update,self.rate);
+	}
+});
